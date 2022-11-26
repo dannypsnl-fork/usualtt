a7e9688d45038b8b8a887c98e717eb50146b14fa
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-11-26 20:25:06 +0800

    type variable is possible to convert to any type of course
    what to do next is avoid **occurs** and **conflict** conversion!
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- lib/Core.ml
+++ lib/Core.ml
@@ -38,6 +38,8 @@ and conv : ty -> ty -> bool =
   fun t1 t2 ->
   match (t1, t2) with
   | (TVar x1, TVar x2) -> String.equal x1 x2
+  | (TVar _x, _t) -> true
+  | (_t, TVar _x) -> true
   | (Arrow (a1, b1), Arrow (a2, b2)) -> conv a1 a2 && conv b1 b2
   | (List a1, List a2) -> conv a1 a2
   | (Bool, Bool) -> true
